f8168bc5d15dd24cd1d3791159fec8640fdc16bd
Daichi Nonaka <jazz29ip@icloud.com>
2018-10-13 14:49:48 +0000

    Install the bootstrap 3

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails'
+gem 'bootstrap-sass', '3.3.1'
 
 group :development do
   gem 'sqlite3'
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module SampleApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
