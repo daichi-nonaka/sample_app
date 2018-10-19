dca101eb420d7a62b6a6aa474d226e43867d6a5a
Daichi Nonaka <jazz29ip@icloud.com>
2018-10-19 12:14:32 +0000

    Install the devise

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 gem 'rails'
 gem 'bootstrap-sass', '3.3.1'
+gem 'devise', '3.4.1'
 
 group :development do
   gem 'sqlite3'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "static_pagesだよー"
   end
 
   def help
   end
   
   def about
+  end
+  
+  def contact
   end
 end
