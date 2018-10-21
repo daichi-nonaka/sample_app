69b3eeba914e2ba2e1a646addfa3e6bebcf2fd86
Daichi Nonaka <jazz29ip@icloud.com>
2018-10-21 10:29:44 +0000

    Finish user registration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
+  private
+
+      def sign_up_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
