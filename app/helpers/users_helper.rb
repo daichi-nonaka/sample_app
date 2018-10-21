76cd9abd2cefe888e5b35bd5973357ed3cd1ffb6
Daichi Nonaka <jazz29ip@icloud.com>
2018-10-21 07:45:39 +0000

    Display a user

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+    
+  # 該当ユーザーのメールアドレスに対応するGravatarの画像URLを返す
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
