3b0092b11d381764250be8c062875ca69292f5ab
Daichi Nonaka <jazz29ip@icloud.com>
2018-10-27 19:32:01 +0000

    Finish user destroy action3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, :except=>[:show]
   before_action :admin_user,     only: :destroy
-
-  def index
-    @users = User.paginate(page: params[:page])
-  end
   
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   
   private
   
-  def admin_user
-    redirect_to(root_path) unless current_user.admin?
-  end
+   def admin_user
+     redirect_to(root_path) unless current_user.admin?
+   end
 end
